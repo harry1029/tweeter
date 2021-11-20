fe9ae5ec7ed8580c31b24cdd2ea44b1cba32f219
Harry Jiang <harry10291993@hotmail.com>
2021-11-19 23:58:42 -0500

    Implement dynamically generated tweets in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);      // calls createTweetElement for each tweet
+      $('#tweets-container').append($tweet);         // takes return value and appends it to the tweets container
+    }
+  }
+
+  /* Takes in tweet object and return a tweet <article> element */
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div>
+          <img src="${tweet.user.avatars}" alt="avatar.png">
+          ${tweet.user.name}
+        </div>
+        ${tweet.user.handle}
+      </header>
+      <div class="tweet-content">
+        <p>${tweet.content.text}</p>
+      </div>
+      <footer>
+        <p>${timeago.format(tweet.created_at)}</p>
+        <div>
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
+
+    return $tweet;
+  }
+
+
+  // Test / driver code (temporary)
+  renderTweets(data);
+});
